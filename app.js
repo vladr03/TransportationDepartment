30d93c80ca03a706ba862344d62a003b4889ec59
Vladislav Raenok <admin@gmail.com>
2022-10-05 15:14:18 +0300

    fixed date. added modules

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const mysql = require("mysql2");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express(); //like an object
 const urlencodedParser = express.urlencoded({ extended: false });
+
+//format date
+hbs.registerHelper('date', require('helper-date'));
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -11,7 +15,7 @@ const connection = mysql.createConnection({
     database: "transportation_departament"
 });
 
-app.set("view engine", "hbs");
+app.set("view engine", "hbs", "pug");
 
 //create account
 app.get("/create", function (req, res) {
@@ -36,6 +40,17 @@ app.get("/edit/:id", function (req, res) {
         if (err) return console.log(err);
         res.render("edit.hbs", {
             customer: data[0]
+        });
+    });
+});
+
+//all orders
+app.get("/orders", function (req, res) {
+    connection.execute("SELECT * FROM transportations", function (err, data) {
+        if (err) return console.log(err);
+        console.log(data);
+        res.render("orders.hbs", {
+            transportations: data
         });
     });
 });
@@ -74,7 +89,7 @@ app.post("/create", urlencodedParser, function (req, res) {
     const password = req.body.password;
     connection.execute("INSERT INTO customers (name, email, number, city, password) VALUES (?, ?, ?, ?, ?)", [name, email, number, city, password], function (err, data) {
         if (err) return console.log(err);
-        res.redirect("/index");
+        res.redirect("/main");
     });
 });
 
